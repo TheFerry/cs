f1309145d1443a38f2ca677424028170dc03d951
semtor <1771391789@qq.com>
2023-03-13 19:28:37 +0800

    fix windows display

--- src/core/run.cpp
+++ src/core/run.cpp
@@ -1,18 +1,24 @@
+#include "../file/file.h"
+#include "flags.h"
 #include <algorithm>
 #include <iostream>
 #include <iterator>
 #include <string>
-#include "flags.h"
-#include "../file/file.h"
+#ifdef _WIN32
+#include <windows.h>
+#endif
 int main(int argc, char *argv[]) {
-  core::Flags& flag = core::Flags::getInstance(argc,argv);
-  if(flag.getFlag()&core::Flags::flag_h){
-    std::cout<<core::helpInfo<<std::endl;
+#ifdef _WIN32
+  SetConsoleOutputCP(CP_UTF8);
+#endif
+  core::Flags &flag = core::Flags::getInstance(argc, argv);
+  if (flag.getFlag() & core::Flags::flag_h) {
+    std::cout << core::helpInfo << std::endl;
     return 0;
   }
   file::Dir d(".");
-  auto buf = d.print(); 
+  auto buf = d.print();
   std::string str;
-  std::copy(buf.begin(),buf.end(),std::back_inserter(str));
-  std::cout<<str;
+  std::copy(buf.begin(), buf.end(), std::back_inserter(str));
+  std::cout << str;
 }
